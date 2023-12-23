d2a5bb4d74267c37ddf02619667d79404cbedebc
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-23 15:52:24 +0100

    Add conditional return (conditional rendering) to "src>components>ListGroup.tsx"

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,8 @@
 function ListGroup() {
   const items = ["London", "Paris", "New York", "Tokyo"];
+
+  if (items.length === 0) return <p>There are no items in the list</p>;
+
   return (
     <>
       <h1>List</h1>
