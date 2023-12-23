6a49d5c3eef2150ad6f769cb4b4a9bf045ad1458
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-23 17:33:47 +0100

    Replace the variable "message" by the function "getMessage" (that could take parameters)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,12 +2,15 @@ function ListGroup() {
   let items = ["London", "Paris", "New York", "Tokyo"];
   items = [];
 
-  const message = items.length === 0 ? "There are no items in the list" : null;
+  //const message = items.length === 0 ? "There are no items in the list" : null;
+  const getMessage = () => {
+    return items.length === 0 ? "There are no items in the list" : null;
+  };
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
