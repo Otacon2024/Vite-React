d1a503d5566e7829c1f94791c4b7d6b4631a3bbb
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-23 17:39:09 +0100

    Add back the ternary operator in the return + Use operator "&&" to make it more concise

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,13 @@ function ListGroup() {
   let items = ["London", "Paris", "New York", "Tokyo"];
   items = [];
 
-  //const message = items.length === 0 ? "There are no items in the list" : null;
-  const getMessage = () => {
-    return items.length === 0 ? "There are no items in the list" : null;
-  };
-
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+
+      {/* { items.length === 0 ? "There are no items in the list" : null } */}
+      {items.length === 0 && "There are no items in the list"}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
