872f4909546943177297eec8230360d318965ad2
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-23 18:51:18 +0100

    Add variable "selectedItem" and ternary condition to apply the correct bootstrap classNames

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["London", "Paris", "New York", "Tokyo"];
+  let selectedItem = -1;
 
   //Event handler
   const handleClick = (event: MouseEvent) => console.log(event);
@@ -11,8 +12,16 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && "There are no items in the list"}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
