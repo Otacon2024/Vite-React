6d14f0bb5512327c0797ffcdc4098e2339b5e72a
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-24 14:10:55 +0100

    Pass to ListGroup jsx element: items={items} heading="Cities"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["London", "Paris", "New York", "Tokyo"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
-  )
+  );
 }
 
 export default App;
