19a63a8a3a1172fb4e1114a433f94f56ebd7c4b9
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-24 18:17:50 +0100

    Remove the ListGroup from "src > App.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["London", "Paris", "New York", "Tokyo"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // let items = ["London", "Paris", "New York", "Tokyo"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /> */}
     </div>
   );
 }
