3c45e66f85160a20867357d3e0b2b598868a8d07
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-24 14:09:58 +0100

    Use props.heading and prop.item in the rendering of "src>components>ListGroup.tsx"

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,21 +7,16 @@ interface Props {
 }
 
 function ListGroup(props: Props) {
-  let items = ["London", "Paris", "New York", "Tokyo"];
-  // Hook
-  //   const arr = useState(-1);
-  //   arr[0]; // variable (selectedItem)
-  //   arr[1]; // updater function (setSelectedItem)
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>props.heading</h1>
 
-      {items.length === 0 && <p>No item found</p>}
+      {props.items.length === 0 && <p>No item found</p>}
 
       <ul className="list-group">
-        {items.map((item, index) => (
+        {props.items.map((item, index) => (
           <li
             className={
               selectedIndex === index
