b3f90002bbabd7b8e752510536eeef6d6316839a
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-25 01:30:54 +0100

    Clean App.tsx and Alert.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-// import ListGroup from "./components/ListGroup";
-
 import Alert from "./components/Alert";
 
 function App() {
-  // let items = ["London", "Paris", "New York", "Tokyo"];
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
-
   return (
     <div>
-      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /> */}
       <Alert>
         Hellow <span>World!</span>
       </Alert>
