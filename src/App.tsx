e1565ed6cdf47d501476bd691c63a0352116ae63
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-25 18:25:21 +0100

    Implement alertVisibility state hook for Alert component visibility control in "src > App.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisibility, setAlertVisibility] = useState(false);
   return (
     <>
-      <Alert>My Alert</Alert>
+      {alertVisibility === true && <Alert>My Alert</Alert>}
       <Button onClick={() => console.log("clicked")}>My button</Button>
     </>
   );
