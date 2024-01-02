40a9adb6d61a2628c5410d67edec635cd176b934
Otacon <79173984+otacon2024@users.noreply.github.com>
2024-01-02 22:58:09 +0000

    Rename the Button exercice App.tsx into AppButton.tsx

---
+++ src/AppButton.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function AppButton() {
+  const [alertVisibility, setAlertVisibility] = useState(false);
+  return (
+    <>
+      {alertVisibility === true && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(!alertVisibility)}>
+        My button
+      </Button>
+    </>
+  );
+}
+
+export default AppButton;
