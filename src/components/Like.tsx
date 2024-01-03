fa2758fcda662cccec9fe3c18a30396a2f633b14
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-03 15:19:51 +0100

    Add useState + onClick event using setStatus

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 const Like = () => {
-  const [status, setStatus] = useState(false);
+  const [status, setStatus] = useState(true);
 
-  if (status) return <AiFillHeart color="#ff6b81" size={20} />;
-  return <AiOutlineHeart size={20} />;
+  if (status)
+    return (
+      <AiFillHeart color="#ff6b81" size={20} onClick={() => setStatus(false)} />
+    );
+  return <AiOutlineHeart size={20} onClick={() => setStatus(true)} />;
 };
 
 export default Like;
