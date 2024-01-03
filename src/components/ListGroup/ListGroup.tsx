d61362f34a396748e200a4b1c7f709f83dd76f86
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-03 14:14:14 +0100

    Refactor ListGroup component to use styled-components
    + Erase inline style demo from ListGroup component

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const List = styled.ul`
-//   list-style: none;
-//   padding: 0;
-// `;
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
 
-// interface ListItemProps {
-//   active: boolean;
-// }
+interface ListItemProps {
+  active: boolean;
+}
 
-// const ListItem = styled.li<ListItemProps>`
-//   padding: 5px 0;
-//   background: ${(props) => (props.active ? "lightblue" : "white")};
-// `;
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "lightblue" : "white")};
+`;
 
 interface Props {
   items: string[];
@@ -29,26 +29,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
 
-      <ul className="list-group" style={{ backgroundColor: "yellow" }}>
-        {items.map((item, index) => (
-          <li
-            className={
-              index === selectedIndex
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            key={item}
-            onClick={() => {
-              setSelectedIndex(index);
-              onSelectItem(item);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-
-      {/* <List>
+      <List>
         {items.map((item, index) => (
           <ListItem
             active={index === selectedIndex}
@@ -61,7 +42,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             {item}
           </ListItem>
         ))}
-      </List> */}
+      </List>
     </>
   );
 }
