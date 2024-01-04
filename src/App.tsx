2b5f29301c8a4fbdd5c12a032302a0edeb10fda1
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-04 10:18:36 +0100

    Create state and give it an object "person"
    + Give it "firstName" and "lastName" properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 
 const App = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const fullName = firstName + " " + lastName;
+  //   const [firstName, setFirstName] = useState("");
+  //   const [lastName, setLastName] = useState("");
+  //   const fullName = firstName + " " + lastName;
 
-  return (
-    <>
-      <div>
-        {fullName}
-        <br />
-        {firstName} + " " + {lastName}
-      </div>
-    </>
-  );
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [isLoading, setLoading] = useState(false);
+
+  return <div></div>;
 };
 
 export default App;
